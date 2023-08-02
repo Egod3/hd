f2439600fdba815f687548fd1dc2982bdd63d7c0
Ezra J Godfrey <ezra.godfrey@gmail.com>
2023-08-02 15:26:20 -0600

    Started to make this a bit more performant.

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 Hexdump written in rust.  This is mainly being done as a learning excercise.
 ## Hexdump
 
+## TODO 
+
+Add support to skip a repeated line even if it isn't all 00.
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use clap::Parser;
+use std::fs::File;
+use std::io;
+use std::io::Read;
 use std::str;
+use std::time::Instant;
 
 // TODO: add a trace32 option that auto-formats the same way d.dump in Trace32 looks.
 #[derive(Parser)]
@@ -24,7 +28,8 @@ fn convert_line(raw_line: &[u8]) -> String {
         let raw_char_as_str = str::from_utf8(&raw_line[i..i + 1]);
         let raw_char = raw_line[i];
         //println!("raw_char_as_str: {}", raw_char_as_str);
-        if raw_char >= 0x20 && raw_char <= 0x7E {
+        if (0x20..=0x7E).contains(&raw_char) {
+            // TODO: Fix this garbage .unwrap() replace with '?'
             conv_line.push_str(raw_char_as_str.unwrap());
         } else {
             conv_line.push('.');
@@ -33,9 +38,78 @@ fn convert_line(raw_line: &[u8]) -> String {
     conv_line
 }
 
-fn main() {
-    println!("Welcome to hexdump (hd)");
+fn line_all_zero(raw_line: &[u8]) -> bool {
+    let mut sum: u64 = 0;
+    for i in raw_line {
+        sum += *i as u64;
+    }
+    sum == 0
+}
 
+fn hexdump(file: String, file_len: usize) -> io::Result<()> {
+    // Open file and read length or default bytes at a time
+    //println!("file_len: {}", file_len);
+    let mut f = File::open(file)?;
+    let mut address: u64 = 0x0;
+    let mut i: usize = 0;
+    let mut is_zero_line_printed = false;
+    let mut is_skip_line_printed = false;
+    let mut line: [u8; 0x10] = [0; 0x10];
+    //let mut prev_line: [u8; 0x10] = [0; 0x10];
+
+    // TODO: handle case at the end where this might overflow the array bounds...
+    while i < file_len {
+        if i + 0x10 > file_len {}
+        f.read_exact(&mut line).expect("Didn't read 0x10 bytes");
+        // Default print no format string provided
+        let conv_line_as_str = convert_line(&line);
+        let _is_line_zero = line_all_zero(&line);
+        if !_is_line_zero || !is_zero_line_printed {
+            let line = format!(
+                    "{:08x}  {:02x} {:02x} {:02x} {:02x} {:02x} {:02x} {:02x} {:02x}  {:02x} {:02x} {:02x} {:02x} {:02x} {:02x} {:02x} {:02x}  |{}|",
+                    address,
+                    line[0],
+                    line[1],
+                    line[2],
+                    line[3],
+                    line[4],
+                    line[5],
+                    line[6],
+                    line[7],
+                    line[8],
+                    line[9],
+                    line[10],
+                    line[11],
+                    line[12],
+                    line[13],
+                    line[14],
+                    line[15],
+                    conv_line_as_str
+                );
+            //let mut line = format!("{:08x}  ", address);
+            //for char in &line[i..i + 16] {
+            //    line = format! {"{} {:02x}", line, char};
+            //}
+            //line = format!("{} |{}|", line, conv_line_as_str);
+            println!("{}", line);
+            if _is_line_zero {
+                is_zero_line_printed = true;
+                is_skip_line_printed = false;
+            } else {
+                is_zero_line_printed = false;
+            }
+        } else if !is_skip_line_printed {
+            println!("*");
+            is_skip_line_printed = true;
+        }
+        address += 0x10;
+        i += 0x10;
+    }
+    Ok(())
+}
+
+fn main() {
+    let now = Instant::now();
     let args = Args::parse();
 
     if let Some(length) = args.length {
@@ -45,39 +119,20 @@ fn main() {
         println!("Value for hex:{hex}");
     }
 
-    // Open file and read length or default bytes at a time
-    // TODO: Fix this garbage .unwrap() replace with '?'
-    let _bytes = std::fs::read(args.file).unwrap();
-    let mut address: u64 = 0x0;
-    let mut i = 0;
-    // TODO: handle case at the end where this might overflow the array bounds...
-    while i < _bytes.len() {
-        // Default print no format string provided
-        // TODO: Fix this garbage .unwrap() replace with '?'
-        let conv_line_as_str = convert_line(&_bytes[i..i + 16]);
-        let line = format!(
-            "{:08x}  {:02x} {:02x} {:02x} {:02x} {:02x} {:02x} {:02x} {:02x}  {:02x} {:02x} {:02x} {:02x} {:02x} {:02x} {:02x} {:02x}  | {}",
-            address,
-            _bytes[i],
-            _bytes[i + 1],
-            _bytes[i + 2],
-            _bytes[i + 3],
-            _bytes[i + 4],
-            _bytes[i + 5],
-            _bytes[i + 6],
-            _bytes[i + 7],
-            _bytes[i + 8],
-            _bytes[i + 9],
-            _bytes[i + 10],
-            _bytes[i + 11],
-            _bytes[i + 12],
-            _bytes[i + 13],
-            _bytes[i + 14],
-            _bytes[i + 15],
-            conv_line_as_str
-        );
-        println!("{}", line);
-        address += 0x10;
-        i += 0x10;
+    let mut _file_len: usize = 0;
+
+    // Check that the file exists before we try and open it.
+    let metadata = std::fs::metadata(args.file.clone());
+    match metadata {
+        Ok(metadata) => {
+            // TODO: Fix this garbage .unwrap() replace with '?'
+            _file_len = metadata.len().try_into().unwrap();
+        }
+        Err(_) => {
+            eprintln!("file does not exist, exiting.");
+            return;
+        }
     }
+    let _ = hexdump(args.file, _file_len);
+    println!("Execution time was: {:#?}", now.elapsed());
 }
